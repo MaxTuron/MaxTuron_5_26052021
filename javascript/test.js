254ae1dcb4ec3d15246ff79c11400122eea29e22
Max Turon <maxx@turon.fr>
2021-06-04 16:50:43 +0200

    Mise en place de l'affichage des articles dynamique.

--- javascript/test.js
+++ javascript/test.js
@@ -6,10 +6,19 @@ fetch("http://localhost:3000/api/teddies/") //Lien vers l'API
         for (let i=0; i<Array(5).length; i++) {
 
             const div = document.createElement('div');
+            div.className='cards';
             section.appendChild(div);
 
+            const img = document.createElement('img');
+            div.appendChild(img);
+            img.src = data[i].imageUrl;
+
+            const lien = document.createElement('a');
+            div.appendChild(lien);
+            lien.src = data[i]._id;
+
             const name = document.createElement('h1');
-            div.appendChild(name);
+            lien.appendChild(name);
             name.innerText = data[i].name;
 
             const price = document.createElement('p');
@@ -19,10 +28,6 @@ fetch("http://localhost:3000/api/teddies/") //Lien vers l'API
             const description = document.createElement('p');
             div.appendChild(description);
             description.innerText = data[i].description;
-
-            const img = document.createElement('img');
-            div.appendChild(img);
-            img.src = data[i].imageUrl;
         }
         console.log(data)
     });
