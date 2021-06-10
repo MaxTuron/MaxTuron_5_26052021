4a4cd28a37dfa6baf0b655f81f073946ec768c93
Max Turon <maxx@turon.fr>
2021-06-10 14:59:05 +0200

    Récupération d'un élement du localstorage.

--- javascript/panier.js
+++ javascript/panier.js
@@ -1,5 +1,18 @@
 const section = document.querySelector('section'); //Défini la ou le code html sera crée
-const panier = [localStorage.getAllItem('article1')];
+const panier = localStorage.getItem('article1');
+
+let objJson = JSON.parse(panier);
+
+const div = document.createElement('div'); //Création d'une div
+div.className='cards'; //Ajout de la classe "cards"
+section.appendChild(div); //Défini l'élément parent "section"
+
+for (let i=0; i<objJson.length; i++) {
+
+    const name = document.createElement('p');
+    div.appendChild(name);
+    name.innerText = objJson[i];
 
 
+}
 console.log(panier);
